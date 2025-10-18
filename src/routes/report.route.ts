cb82a919347828237ed76378a62f7d03f5d58309
Vinson Luckianto <luckiantovinson@gmail.com>
2025-10-18 16:05:29 +0700

    fix export data excel

--- src/routes/report.route.ts
+++ src/routes/report.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { imageUplouder } from '../utils/uplouder';
-import { createReport, deleteReport, updateReport, updateReportStatus } from '../controllers/report.controller';
+import { createReport, deleteReport, exportExcelReport, updateReport, updateReportStatus } from '../controllers/report.controller';
 
 export const ReportRouter: Router = Router();
 
@@ -8,3 +8,4 @@ ReportRouter.post('/', imageUplouder.single('image'), createReport);
 ReportRouter.put('/:id',imageUplouder.single('image'), updateReport);
 ReportRouter.delete('/:id', deleteReport);
 ReportRouter.post('/status/:id', updateReportStatus); 
+ReportRouter.post('/export', exportExcelReport);
